5265786a40cf2843506c9f166d1279cb5aa8f2d5
Fernando Martinez <fernando817mm@gmail.com>
2021-07-15 17:30:59 -0500

    bug fixed

--- src/App.js
+++ src/App.js
@@ -8,13 +8,23 @@ function App() {
 
   const [data, setData] = useState([]);
 
+  const [number, setNumber] = useState(1);
+
+  document.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const sel = document.querySelector('select');
+    const num = sel.value;
+    setNumber(num);
+  })
+
+
   useEffect(() => {
-    axios.get(`https://api.nasa.gov/planetary/apod?api_key=CFj2RQgRwOB6Te8ybChOawpH2BmRkAHdBzHAGJGR&count=5`)
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=CFj2RQgRwOB6Te8ybChOawpH2BmRkAHdBzHAGJGR&count=${number}`)
       .then(res => {
         setData(res.data);
       })
       .catch(err => alert(err));
-  }, [])
+  }, [number])
 
   return (
     <div className="App">
